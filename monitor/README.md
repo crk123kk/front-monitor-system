979123be4e91041b4bde094a07dbb5d699d398c1
crk <1973657048@qq.com>
2021-04-27 06:52:13 +0800

    init project

--- monitor/README.md
+++ monitor/README.md
@@ -1,3 +1,9 @@
 #### 项目介绍
 
     通过webpack实现监控采集脚本
+
+#### 遇到的问题
+
+    webpack和webpack-dev-server版本不兼容：
+
+        webpack-dev-server": "^3.11.2" 需要通过 webpack serve --open指令启动
--- monitor/webpack.config.js
+++ monitor/webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
 
+// webpack 打包项目的 HtmlWebpackPlugin生成产出HTML文件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// user-agent 把浏览器的UserAgent变成一个对象
+
 module.exports = {
+  // 入口文件
   entry: './src/index.js',
+  // 上下文目录
   context: process.cwd(),
+  // 开发模式
   mode: 'development',
   output: {
+    // 输出目录
     path: path.resolve(__dirname, 'dist'),
+    // 输出文件名
     filename: 'monitor.js',
   },
   devServer: {
+    // devServer静态文件根目录
     contentBase: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    // 自动打包HTML文件
     new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: 'head',
